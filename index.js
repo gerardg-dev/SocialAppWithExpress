ede2ef4d2bc999fa043c9e6e28586c8e9c5402ac
“GerardoGallegos” <gerardg.dev@gmail.com>
2017-12-26 17:27:47 -0600

    Added our cookie session and initalized it

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
-require('./services/passport')
+require('./models/User');
+require('./services/passport');
+
+// index.js is our server app entry point
+// here goes all of our code initialization
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30Days in Milliseconds
+    keys: [keys.cookieKey1, keys.cookieKey2, keys.cookieKey3]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
